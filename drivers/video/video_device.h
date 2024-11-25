4080680bc0eb309d50a64802da189a3147757b64
Phi Bang Nguyen <phibang.nguyen@nxp.com>
2024-11-25 23:14:09 +0100

    drivers: video: Introduce video device and its surroundings
    Introduce a new video device structure which represents the root device
    in a video pipeline. A root device is the one that represents the whole
    pipeline to communicate with the application. It is usually the device
    assigned to the "zephyr,camera" chosen node.
    
    Root device then needs to hold a video device structure and register it
    with the video framework.
    
    Signed-off-by: Phi Bang Nguyen <phibang.nguyen@nxp.com>

---
+++ drivers/video/video_device.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2024 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_VIDEO_DEVICE_H_
+#define ZEPHYR_INCLUDE_VIDEO_DEVICE_H_
+
+#include <zephyr/device.h>
+
+struct video_device {
+	int ind;
+	const struct device *dev;
+};
+
+int video_register_vdev(struct video_device *vdev, const struct device *dev);
+
+void video_unregister_vdev(struct video_device *vdev);
+
+struct video_device *video_find_vdev(const struct device *dev);
+
+#endif /* ZEPHYR_INCLUDE_VIDEO_DEVICE_H_ */
